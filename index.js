996fed614edcae74dcaa2fbd3a3781dcb2852a9a
Nikita Gupta <63334527+NIKITA-19@users.noreply.github.com>
2021-09-06 10:48:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser=require('body-parser');
 const app=express();
 const logapp=express();
 const path=require('path');
+const PORT = process.env.PORT ||5000;
 app.use('/home',express.static(path.join(__dirname, 'homepage')));
 app.use('/adopt',express.static(path.join(__dirname, 'adoptpage')));
 app.use('/',express.static(path.join(__dirname, 'loginpage')));
@@ -31,6 +32,6 @@ app.get('/diseases',function(req,res){
 app.get('/product',function (req,res) {
   res.sendFile(__dirname+'/product/index.html');
 })
-app.listen(443,function () {
+app.listen(PORT,function () {
   console.log("Server is running");
 })
